838d0651bf25ff96446d8e5475a32944ee578bba
Nguyễn Hoàng Minh <mingu03@yandex.com>
2023-12-17 21:13:15 +0700

    added logics and useless information display to use more memory and watse time.

--- compile.c
+++ compile.c
@@ -1,11 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
     printf("Compiler: g++\n");
-    printf("Compiling *.cpp -> .o\n");
-    system("g++ -c *.cpp");
-    printf("Linking .o files to executable XLGD\n");
-    system("g++ *.o -o XLGD");
+    printf("Compiling *.cpp -> .o: ");
+    char command1[] = "g++ -c *.cpp";
+    puts(command1);
+    if (system(command1) == 0)
+    {
+        char command2[] = "g++ *.o -o XLGD";
+        printf("Linking .o files to executable XLGD: ");
+        puts(command2);
+        if (system(command2) == 0)
+        {
+            printf("Execute the program? (y/n): ");
+            char choice;
+            fflush(stdin);
+            scanf("%c", &choice);
+            if (choice == 'y')
+                system("XLGD");
+            return 0;
+        }
+        // if fail to execute command system, exit if
+    }
+    // if fail to execute command, exit if
+    printf("Fail to execute command, check console, maybe it will display something\n");
     return 0;
 }
